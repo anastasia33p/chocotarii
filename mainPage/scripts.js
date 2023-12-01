ecb72aa14040542a827f2ecc6c3911e36032efd5
anastasia33p <115028090+anastasia33p@users.noreply.github.com>
2023-12-01 23:31:50 +0300

    Update scripts.js

--- mainPage/scripts.js
+++ mainPage/scripts.js
@@ -1,11 +1,11 @@
 let cardData = [
-    { imageUrl: '../svg/array_candies/candy_1.svg', altText: 'Изображение 1' },
-    { imageUrl: '../svg/array_candies/candy_2.svg', altText: 'Изображение 2' },
-    { imageUrl: '../svg/array_candies/candy_3.svg', altText: 'Изображение 3' },
-    { imageUrl: '../svg/array_candies/candy_4.svg', altText: 'Изображение 4' },
-    { imageUrl: '../svg/array_candies/candy_5.svg', altText: 'Изображение 5' },
-    { imageUrl: '../svg/array_candies/candy_6.svg', altText: 'Изображение 6' },
-    { imageUrl: '../svg/array_candies/candy_7.svg', altText: 'Изображение 7' },
+    { imageUrl: 'svg/array_candies/candy_1.svg', altText: 'Изображение 1' },
+    { imageUrl: 'svg/array_candies/candy_2.svg', altText: 'Изображение 2' },
+    { imageUrl: 'svg/array_candies/candy_3.svg', altText: 'Изображение 3' },
+    { imageUrl: 'svg/array_candies/candy_4.svg', altText: 'Изображение 4' },
+    { imageUrl: 'svg/array_candies/candy_5.svg', altText: 'Изображение 5' },
+    { imageUrl: 'svg/array_candies/candy_6.svg', altText: 'Изображение 6' },
+    { imageUrl: 'svg/array_candies/candy_7.svg', altText: 'Изображение 7' },
 ];
 
 let cardContainer = document.getElementById('cardContainer');
